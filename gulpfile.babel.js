69d09f679de068017174a117c1b94e5a6963309a
toshi0607 <s.toshi0607@gmail.com>
2016-11-05 18:21:04 +0900

    add client app with webpack

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,16 +3,26 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 import del from 'del';
-import {exec} from 'child_process';
 import eslint from 'gulp-eslint';
+import webpack from 'webpack-stream';
+import webpackConfig from './webpack.config.babel';
 
 const paths = {
   allSrcJs: 'src/**/*.js',
+  serverSrcJs: 'src/server/**/*.js?(x)',
+  sharedSrcJs: 'src/shared/**/*.js?(x)',
+  clientEntryPoint: 'src/client/app.js',
   gulpFile: 'gulpfile.babel.js',
+  webpackFile: 'webpack.config.babel.js',
   libDir: 'lib',
+  distDir: 'dist',
+  clientBundle: 'dist/client-bundle.js?(.map)',
 };
 
-gulp.task('clean', () => del(paths.libDir));
+gulp.task('clean', () => del([
+  paths.libDir,
+  paths.clientBundle,
+]));
 
 gulp.task('build', ['lint', 'clean'], () =>
   gulp.src(paths.allSrcJs)
@@ -20,12 +30,11 @@ gulp.task('build', ['lint', 'clean'], () =>
     .pipe(gulp.dest(paths.libDir))
 );
 
-gulp.task('main', ['build'], (callback) => {
-  exec(`node ${paths.libDir}`, (error, stdout) => {
-    console.log(stdout);
-    return callback(error);
-  });
-});
+gulp.task('main', ['lint', 'clean'], () =>
+  gulp.src(paths.clientEntryPoint)
+    .pipe(webpack(webpackConfig))
+    .pipe(gulp.dest(paths.distDir))
+);
 
 gulp.task('watch', () => {
   gulp.watch(paths.allSrcJs, ['main']);
@@ -37,6 +46,7 @@ gulp.task('lint', () =>
   gulp.src([
     paths.allSrcJs,
     paths.gulpFile,
+    paths.webpackFile,
   ])
     .pipe(eslint())
     .pipe(eslint.format())
--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import Dog from './dog';
+import Dog from '../shared/dog';
 
 const str = 'ES6';
 console.log(`Hello ${str}`);
