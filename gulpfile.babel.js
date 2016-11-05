dcba37973db668401dbcfe31a251bc77d3b09604
toshi0607 <s.toshi0607@gmail.com>
2016-11-06 01:17:13 +0900

    testing with mocha, chai and sion

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,6 +5,7 @@ import babel from 'gulp-babel';
 import del from 'del';
 import eslint from 'gulp-eslint';
 import webpack from 'webpack-stream';
+import mocha from 'gulp-mocha';
 import webpackConfig from './webpack.config.babel';
 
 const paths = {
@@ -17,6 +18,7 @@ const paths = {
   libDir: 'lib',
   distDir: 'dist',
   clientBundle: 'dist/client-bundle.js?(.map)',
+  allLibTests: 'lib/test/**/*.js',
 };
 
 gulp.task('clean', () => del([
@@ -30,7 +32,7 @@ gulp.task('build', ['lint', 'clean'], () =>
     .pipe(gulp.dest(paths.libDir))
 );
 
-gulp.task('main', ['lint', 'clean'], () =>
+gulp.task('main', ['test'], () =>
   gulp.src(paths.clientEntryPoint)
     .pipe(webpack(webpackConfig))
     .pipe(gulp.dest(paths.distDir))
@@ -51,4 +53,9 @@ gulp.task('lint', () =>
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError())
+);
+
+gulp.task('test', ['build'], () =>
+  gulp.src(paths.allLibTests)
+    .pipe(mocha())
 );
